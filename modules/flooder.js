c66d118d0427ae94b5bbd6b7c659ff001d79a1f4
kosmodrey <kosmodrey@yandex.ru>
2015-07-27 19:35:17 +0300

    Add simple flood protection module

---
+++ modules/flooder.js
@@ -0,0 +1,49 @@
+/*
+
+  Flooder
+  Simple flood protection module
+
+  CONFIG:
+
+  flood: {
+    interval: 1, // in seconds
+    message: 'Flood message.' // message
+  }
+  
+  NOTE: Received Telegram message time accuracy is one second!
+
+*/
+
+// Store users
+var userList = {};
+
+// Export bot module
+module.exports = function(bot) {
+  // Load config data
+  var cfg = bot.cfg.flood || {};
+  var interval = Number(cfg.interval) || 1;
+  var text = cfg.message || 'Too many messages from you. Please, try later...';
+  // Create message modifier
+  bot.mod('message', function(msg) {
+    var id = msg.from.id, user = userList[id];
+    console.log(msg.date);
+    var now = new Date(msg.date);
+    if (user) {
+      var diff = now - user.lastTime;
+      user.lastTime = now;
+      console.log(diff);
+      if (diff <= interval) {
+        if (!user.flood) {
+          if (text) bot.sendMessage(id, text);
+          user.flood = true;
+        }
+        msg = {};
+      } else {
+        user.flood = false;
+      }
+    } else {
+      userList[id] = { lastTime: now };
+    }
+    return msg;
+  });
+};
