ca1004e2a6358a8f2b0bfcf59505fd3749172697
kosmodrey <kosmodrey@yandex.ru>
2016-05-02 12:30:42 +0300

    Ask module

---
+++ modules/ask.js
@@ -0,0 +1,39 @@
+/*
+  Name: Ask
+  Description: Get direct answers from users!
+*/
+
+// Store user list
+const userList = {};
+
+module.exports = bot => {
+
+  // On every text message
+  bot.on('text', msg => {
+
+    let id = msg.chat.id,
+      ask = userList[id];
+    
+    // If no question, then it's a regular message
+    if (!ask) return;
+
+    // Delete user from list and send custom event
+    delete userList[id];
+    bot.event('ask.' + ask, msg, this);
+  
+  });
+  
+  // Before call sendMessage method
+  bot.on('sendMessage', args => {
+
+    let id = args[0],
+      opt = args[2] || {};
+
+    let ask = opt.ask;
+
+    // If "ask" in options, add user to list
+    if (ask) userList[id] = ask;
+  
+  });
+
+};
