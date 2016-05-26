a7e053c8ddb9f4b2cd8535c83e57dd6dab0003be
kosmodrey <kosmodrey@yandex.ru>
2016-05-26 17:29:33 +0300

    Update ngrok webhook example

--- examples/ngrok-webhook.js
+++ examples/ngrok-webhook.js
@@ -3,6 +3,7 @@
 const TeleBot = require('../');
 const ngrok = require('ngrok');
 
+const token = '-PASTEYOURTELEGRAMBOTAPITOKENHERE-';
 const host = '0.0.0.0';
 const port = 443;
 
@@ -11,8 +12,7 @@ ngrok.connect(port, (error, url) => {
   if (error) return console.error('[ngrok] error:', error);
 
   const bot = new TeleBot({
-    token: '-PASTEYOURTELEGRAMBOTAPITOKENHERE-',
-    webhook: { url, host, port }
+    token, webhook: { url, host, port }
   });
 
   bot.on('text', msg => bot.sendMessage(msg.from.id, msg.text));
