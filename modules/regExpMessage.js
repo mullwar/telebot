e61950a4850b0101a8fdd92e50e912413588dc9a
mullwar <mullwar@gmail.com>
2017-05-01 12:46:06 +0300

    Build-in module: regExpMessage

---
+++ modules/regExpMessage.js
@@ -0,0 +1,24 @@
+module.exports = (bot, config) => {
+
+    bot.mod('text', (data) => {
+        const { message, props } = data;
+        const text = message.text;
+
+        let promise = Promise.resolve();
+
+        for (let eventType of bot.eventList.keys()) {
+            if (eventType instanceof RegExp) {
+                const match = text.match(eventType);
+                if (match) {
+                    props.match = match;
+                    promise = promise.then(() => bot.event(eventType, message, props));
+                }
+            }
+        }
+        
+        data.promise = promise;
+
+        return data;
+    });
+
+};
