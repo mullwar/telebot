3c06bcc5be0b69cfb29ba663b59e821f417da8d5
mullwar <mullwar@gmail.com>
2017-05-11 18:36:41 +0300

    Adds ESLint file

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": "off",
+        "indent": ["error", 4],
+        "semi": ["error", "always"]
+    }
+};
