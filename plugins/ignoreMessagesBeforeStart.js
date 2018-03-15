40b2f9fcd95fb04174ef3b00c4eb2eee81692fc4
Shemesh <shemeshi@gmail.com>
2018-03-15 11:40:51 +0200

    add "ignore messages before start" plugin

---
+++ plugins/ignoreMessagesBeforeStart.js
@@ -0,0 +1,21 @@
+var startDate;
+
+module.exports = {
+
+id: 'ignoreMessagesBeforeStart',
+
+    plugin(bot) {
+
+        bot.on('start', () => {
+            startDate = Date.now();
+        });
+
+        bot.mod('message', (data) => {
+            if (data.message.date*1000 < startDate) {
+                data.message = {};
+            }
+
+            return data;
+        });
+    }
+};
