7e8a7598794c9114cd375693a9d8586933e5bbd0
YouTwitFaceTG <32808683+YouTwitFaceTG@users.noreply.github.com>
2018-03-29 09:55:35 -0400

    Update permissions.js

--- plugins/permissions.js
+++ plugins/permissions.js
@@ -2,17 +2,24 @@ module.exports = {
 
     id: 'permissions',
     defaultConfig: {
-        allowedUserNames: [],
+        allowedUserIds: [],
         message: '⛔ you are not authorized'
     },
 
     plugin(bot, pluginConfig) {
 
         bot.mod('message', (data) => {
-            const userId = data.message.from.id;
-            if (pluginConfig.allowedUserNames.indexOf(data.message.from.username) < 0) {
-                data.message = {};
-                bot.sendMessage(userId, pluginConfig.message);
+            if (data.message.chat.type === 'channel') {
+                const chatId = data.message.chat.id;
+                bot.sendMessage(userId, pluginConfig.message).then(() => {
+                    bot.leaveChat(chatId);
+                });
+            } else {
+                const userId = data.message.from.id;
+                if (pluginConfig.allowedUserNames.includes(userId) < 0) {
+                    data.message = {};
+                    bot.sendMessage(userId, pluginConfig.message);
+                }
             }
 
             return data;
