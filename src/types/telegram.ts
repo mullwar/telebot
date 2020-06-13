df661e9f81ddf669285b7aeb65e971e1a0d855b2
mullwar <mullwar@protonmail.com>
2020-06-13 12:11:22 +0300

    🗂 PassportElementError types

--- src/types/telegram.ts
+++ src/types/telegram.ts
@@ -43,8 +43,6 @@ export type GameHighScore = {
     score: number;
 };
 
-export type PassportElementError = any; // TODO
-
 export type UpdateTypes = string[]; // TODO: keyof Update
 
 export type ChatId = number | string;
@@ -52,6 +50,86 @@ export type ChatId = number | string;
 export type InputFile = string;
 
 export type BotInputFile = InputFile | string;
+
+export type PassportElementError =
+    PassportElementErrorDataField |
+    PassportElementErrorFrontSide |
+    PassportElementErrorReverseSide |
+    PassportElementErrorSelfie |
+    PassportElementErrorFile |
+    PassportElementErrorFiles |
+    PassportElementErrorTranslationFile |
+    PassportElementErrorTranslationFiles |
+    PassportElementErrorUnspecified;
+
+export type PassportElementErrorDataField = {
+    source: "data";
+    type: "personal_details" | "passport" | "driver_license" | "identity_card" | "internal_passport" | "address";
+    field_name: string;
+    data_hash: string;
+    message: string;
+};
+
+export type PassportElementErrorFrontSide = {
+    source: "front_side";
+    type: "passport" | "driver_license" | "identity_card" | "internal_passport";
+    file_hash: string;
+    message: string;
+};
+
+export type PassportElementErrorReverseSide = {
+    source: "reverse_side";
+    type: "driver_license" | "identity_card";
+    file_hash: string;
+    message: string;
+};
+
+export type PassportElementErrorSelfie = {
+    source: "selfie";
+    type: "passport" | "driver_license" | "identity_card" | "internal_passport";
+    file_hash: string;
+    message: string;
+};
+
+export type PassportElementErrorFile = {
+    source: "file";
+    type: "utility_bill" | "bank_statement" | "rental_agreement" | "passport_registration" | "temporary_registration";
+    file_hash: string;
+    message: string;
+};
+
+
+export type PassportElementErrorFiles = {
+    source: "files";
+    type: "utility_bill" | "bank_statement" | "rental_agreement" | "passport_registration" | "temporary_registration";
+    file_hashes: string[];
+    message: string;
+};
+
+export type PassportElementErrorTranslationFile = {
+    source: "translation_file";
+    type: "passport" | "driver_license" | "identity_card" |
+        "internal_passport" | "utility_bill" | "bank_statement" | "rental_agreement" |
+        "passport_registration" | "temporary_registration";
+    file_hash: string;
+    message: string;
+};
+
+export type PassportElementErrorTranslationFiles = {
+    source: "translation_files";
+    type: "passport" | "driver_license" | "identity_card" |
+        "internal_passport" | "utility_bill" | "bank_statement" | "rental_agreement" |
+        "passport_registration" | "temporary_registration";
+    file_hashes: string[];
+    message: string;
+};
+
+export type PassportElementErrorUnspecified = {
+    source: "unspecified";
+    type: string;
+    element_hash: string;
+    message: string;
+};
 
 export type InputMessageContent =
     InputTextMessageContent |
