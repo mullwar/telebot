fc6ac9236e3af42e2d459c62ac67f95135b5c2cd
xyhtac <73641145+xyhtac@users.noreply.github.com>
2023-07-12 21:22:13 +0300

    Update edit-caption.js
    store var LastMessage separertely to prevent race condition between users

--- examples/edit-caption.js
+++ examples/edit-caption.js
@@ -1,7 +1,7 @@
 const TeleBot = require('../');
 const bot = new TeleBot('TELEGRAM_BOT_TOKEN');
 
-var lastMessage;
+var lastMessage = {};
 var photoUrl = 'https://telegram.org/img/tl_card_destruct.gif';
 
 bot.on('/start', msg => {
@@ -11,7 +11,7 @@ bot.on('/start', msg => {
         msg.from.id, photoUrl, {caption: 'This is a default caption.'}
     ).then(re => {
         // Get message id and chat
-        lastMessage = [msg.from.id, re.result.message_id];
+        lastMessage[msg.from.id] = [msg.from.id, re.result.message_id];
         bot.sendMessage(msg.from.id, 'Now set a new caption using /edit <caption>');
     });
 
